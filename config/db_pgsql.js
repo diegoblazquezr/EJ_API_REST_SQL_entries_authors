e8d9d418a4a3f80c3e34be6e5d3d8170f0b6a711
diegoblazquezr <diegoblazquezr@gmail.com>
2024-06-23 19:42:48 +0200

    fixed pool

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -4,24 +4,24 @@ require('dotenv').config()
 
 // Datos de conexión
 
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    port: '5432',
-    database: 'postgres',
-    password: '123456'
-});
-
 // const pool = new Pool({
-//     host: 'dpg-cpoqo6iju9rs738ufcc0-a.frankfurt-postgres.render.com',
-//     user: 'userrenderpostgresql',
-//     password: 'YbK3LLwigCfRy70zIwddDSSmm29TKgY8',
-//     database: 'BBDD-Authors-Entries',
+//     host: 'localhost',
+//     user: 'postgres',
 //     port: '5432',
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
+//     database: 'postgres',
+//     password: '123456'
 // });
+
+const pool = new Pool({
+    host: 'dpg-cpoqo6iju9rs738ufcc0-a.frankfurt-postgres.render.com',
+    user: 'userrenderpostgresql',
+    password: 'YbK3LLwigCfRy70zIwddDSSmm29TKgY8',
+    database: 'BBDD-Authors-Entries',
+    port: '5432',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 // pool.connect()
 //     .then(() => console.log('Conexión exitosa'))
